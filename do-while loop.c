9cf3b3ffa96ee65b794529ebd3627d292a6f1502
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-02-23 14:37:26 +0530

    do while loop eg1

---
+++ do-while loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 2; // Initialization expression
+
+	do
+	{
+		// loop body
+		printf( "Thundersoft\n");	
+
+		// update expression
+		i++;
+
+	} while (i < 1); // test expression
+
+	return 0;
+}
+
+
