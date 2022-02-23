ea58506d1b07d7bb2c7c3a434cc1efa3b7ef4c48
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-02-23 14:02:56 +0530

    for_loop2.c

---
+++ for_loop2.c
@@ -0,0 +1,18 @@
+/*Also, we can skip the initial value expression, 
+condition and/or increment by adding a semicolon.
+
+
+For example:
+*/
+#include<stdio.h>
+int main()
+{
+int i=0;
+int max = 10;
+for (; i < max; i++)
+{
+  		printf("%d\n", i);
+	}
+	return 0;
+}
+
