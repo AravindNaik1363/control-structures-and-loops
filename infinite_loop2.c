1916de4f60bea62d4d3e855538b6d5f38231f796
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-02-23 14:26:05 +0530

    infinite loop2 in while

---
+++ infinite_loop2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	short int i=1;//range is from -32768 to +32767
+	while(1)//condition is true enters loop //while(i) (finite loop)
+	{
+		printf("i value :%d\n",i );
+		i++;
+	}
+	return 0;
+}
+
+
