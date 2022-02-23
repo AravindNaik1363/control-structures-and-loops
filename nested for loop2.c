e60c0616451986ff10488ea5981cda22eadaed45
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-02-23 14:42:40 +0530

    printing patterns using nested for loops

---
+++ nested for loop2.c
@@ -0,0 +1,20 @@
+//Nested loops(pattern program)
+
+#include <stdio.h>
+int main() 
+{
+   int i, j, rows;
+   printf("Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) 
+   {
+      for (j = 1; j <= 3; ++j) 
+      {
+         printf("* ");
+      }
+      printf("\n");
+   }
+   return 0;
+}
+
+
