383e21ce912a78a07290c44c58757adc98fd51f4
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-02-23 14:48:12 +0530

    without giving braces for if and else condition

---
+++ if-while2.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h> 
+int  main()
+{
+	if(7)
+		printf(“hi\n”);
+	else
+		printf(“all\n”);
+		printf(“bye\n”);
+	return 0;
+}
+
+//When no braces are given only 1st statement 
+//belongs to that condition
