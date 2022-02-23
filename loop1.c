eed5e3cd0006f142bd608f1fe7f070f45877f653
AravindNaik1363 <aravind.naik@thundersoft.com>
2022-02-23 10:13:03 +0530

    usage of loops

---
+++ loop1.c
@@ -0,0 +1,15 @@
+// C program to illustrate need of loops
+#include <stdio.h>
+
+int main()
+{
+	printf( "Thundersoft\n");
+	printf( "Thundersoft\n");
+	printf( "Thundersoft\n");
+	printf( "Thundersoft\n");
+	printf( "Thundersoft\n");
+	return 0;
+}
+
+
+
