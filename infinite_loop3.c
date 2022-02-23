96e9bc85b29bbaca2309c2a74360f3f361ba64d5
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-02-23 14:28:15 +0530

    infinite loop3 eg in while

---
+++ infinite_loop3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	signed char ch=120;//range is from -128 to +127
+	while(++ch)//condition is true enters loop 
+	{
+		printf("ch value :%d\n",ch );
+	
+	}
+	return 0;
+}
+
+
